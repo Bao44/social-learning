51564a09624083f92822ae30db5c0872a7bacab0
Bao44 <tqbao44@gmail.com>
2025-06-25 23:38:18 +0700

    style: setup shadcn UI

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,10 +1,23 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Welcome to My Next.js App!</h1>
-      <p className="mt-4 text-lg">This is a simple example of a Next.js application.</p>
+      <Button className="bg-red-500 hover:bg-red-600 text-white rounded-xl h-30">
+        Click me
+      </Button>
+      <p className="mt-4 text-lg">
+        This is a simple example of a Next.js application.
+      </p>
+
+      <Avatar className="h-20 w-20">
+        <AvatarImage
+          src="https://github.com/shadcn.png"
+        />
+      </Avatar>
       <Image
         src="/next.svg"
         alt="Next.js Logo"
