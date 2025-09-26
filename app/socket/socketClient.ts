f9b6eb7b9ba59a7f721c6ecee124293946ca9677
Bao44 <tqbao44@gmail.com>
2025-09-26 15:36:51 +0700

    fix: socket

---
+++ app/socket/socketClient.ts
@@ -0,0 +1,15 @@
+import { io } from 'socket.io-client';
+
+let socket: any = null;
+
+export const getSocket = () => {
+  if (!socket) {
+    socket = io('http://10.0.2.2:5000', {
+      transports: ['websocket'],
+      reconnection: true,
+      reconnectionAttempts: 5,
+      reconnectionDelay: 1000,
+    });
+  }
+  return socket;
+};
