fa7c1e3399f8bcaed8d674c1d469971fe310733a
Nguyen Thanh Thuan <nguyenthuan020010@gmail.com>
2025-10-02 20:43:26 +0700

    fix: copy api learning from web to app

---
+++ app/src/api/learning/listening/route.ts
@@ -0,0 +1,18 @@
+import api from '../../../../lib/api';
+
+export const listeningService = {
+    // Get listening exercise by id
+    getListeningExerciseById: async (id: string) => {
+        const response = await api.get(`/api/learning/listening/listening-exercises/${id}`);
+        return response.data;
+    },
+
+    // Generate listening exercise by AI
+    generateListeningExerciseByAI: async (level_slug: string, topic_slug: string) => {
+        const response = await api.post(`/api/bot-cover-learning/generate-listening-exercise`, {
+            level_slug,
+            topic_slug
+        });
+        return response.data;
+    }
+};
